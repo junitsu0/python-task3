4b386c70cb50804e92b4321db4f779b0acaf57f5
Justin <junitsu00@gmail.com>
2022-07-17 19:39:56 -0500

    first commit and last

---
+++ Task3.py
@@ -0,0 +1,34 @@
+# --- Honestly I don't have time for this and forgot the 101 things I did before class.
+# --- Though I generally disagree with graded assignments before class would even begin,
+# --- or the topic not even covered, sincerely this was a time issue. I don't have enough.
+# --- Also, I did not want to just cut/paste some answer in without knowing how it works.
+# --- So, although I don't like the idea of missing an entire assignment of points,
+# --- because I genuinely want to perform well and learn all I can,
+# --- I will have to take the hit on this one and just be stellar afterwards.
+
+
+
+# Problem 1 ---------------------
+
+def hello_name(user_name):
+    user_name = (USERNAME)
+    print(hello_ + user_name + "!")
+
+# Problem 2 ---------------------
+
+def first_odds():
+
+
+# Problem 3 ---------------------
+
+def max_num_in_list(a_list):
+
+
+# Problem 4 ---------------------
+
+def is_leap_year(a_year):
+
+
+# Problem 5 ---------------------
+
+def is_consecutive(a_list):
