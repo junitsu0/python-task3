6a3b4d71fc6e1beea567a957f95338df6a8c8156
Justin <junitsu00@gmail.com>
2022-08-03 23:28:00 -0500

    just got around to this, had forgotten

--- Task3.py
+++ Task3.py
@@ -1,34 +1,39 @@
-# --- Honestly I don't have time for this and forgot the 101 things I did before class.
-# --- Though I generally disagree with graded assignments before class would even begin,
-# --- or the topic not even covered, sincerely this was a time issue. I don't have enough.
-# --- Also, I did not want to just cut/paste some answer in without knowing how it works.
-# --- So, although I don't like the idea of missing an entire assignment of points,
-# --- because I genuinely want to perform well and learn all I can,
-# --- I will have to take the hit on this one and just be stellar afterwards.
-
-
-
 # Problem 1 ---------------------
 
 def hello_name(user_name):
-    user_name = (USERNAME)
-    print(hello_ + user_name + "!")
+    print("hello_" + user_name.upper() + "!")
+
+hello_name("username")
 
 # Problem 2 ---------------------
 
-def first_odds():
-
+def first_odds(x):
+    for x in range(1,101):
+        if x % 2 == 1:
+            print(x)
+first_odds(x)
 
 # Problem 3 ---------------------
 
 def max_num_in_list(a_list):
+    print(max(a_list))
 
-
+max_num_in_list([5, 8, 15, 62, 27, 50, 31, 46])
 # Problem 4 ---------------------
 
 def is_leap_year(a_year):
+    if a_year % 400 == 0 and a_year % 100 == 0:
+        return True
+    elif a_year % 4 == 0 and a_year % 100 != 0:
+        return True
+    else:
+        return False
 
+is_leap_year(144)
 
 # Problem 5 ---------------------
 
 def is_consecutive(a_list):
+    return sorted(a_list) == list(range(min(a_list), max(a_list)+1))
+
+is_consecutive([2,3,4,5,6,7])
